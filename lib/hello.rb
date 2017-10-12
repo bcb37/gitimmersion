eb8be58372f436d22a806788a3240b9f0b87243b
Ben Blanchard <benjamin.blanchard@gmail.com>
2017-10-12 18:03:54 -0400

    Hello uses greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,4 +1,7 @@
+require 'greeter'
+
 # Default is "World"
-#Author: Ben Blanchard (benjamin.blanchard@gmail.com)
 name = ARGV.first || "World"
-puts "Hello, #{name}!"
+
+greeter = Greeter.new(name)
+puts greeter.greet
